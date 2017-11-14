fb2b3e04cda643e476cdcc2acaa2d254e0284ee8
Luis <luisdelae@gmail.com>
2017-11-14 13:09:08 -0600

    Functions

--- TSDemo-LibraryManagerApp/app.js
+++ TSDemo-LibraryManagerApp/app.js
@@ -1,29 +1,24 @@
-﻿function GetAllBooks() {
+function GetAllBooks() {
     let books = [
         { title: 'Ulysses', author: 'James Joyce', available: true },
         { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
         { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true },
         { title: 'Moby Dick', author: 'Herman Melville', available: true }
     ];
-
     return books;
 }
-
-function LogsFirstAvailable(books): void {
-    let numberOfBooks: number = books.length;
-    let firstAvailable: string = '';
-
+function LogsFirstAvailable(books) {
+    let numberOfBooks = books.length;
+    let firstAvailable = '';
     for (let currentBook of books) {
-
         if (currentBook.available) {
             firstAvailable = currentBook.title;
             break;
         }
     }
-
     console.log('Total Books: ', numberOfBooks);
     console.log('First Available: ', firstAvailable);
 }
-
 const allBooks = GetAllBooks();
 LogsFirstAvailable(allBooks);
+//# sourceMappingURL=app.js.map
