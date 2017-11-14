0e455af1af1d7394e548ceaab25295cc69755a05
Luis <luisdelae@gmail.com>
2017-11-14 13:44:02 -0600

    Arrow functions

--- TSDemo-LibraryManagerApp/app.js
+++ TSDemo-LibraryManagerApp/app.js
@@ -1,9 +1,9 @@
 function GetAllBooks() {
     let books = [
-        { title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
-        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
-        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
-        { title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
+        { id: 1, title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+        { id: 2, title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { id: 3, title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+        { id: 4, title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
     ];
     return books;
 }
@@ -43,6 +43,11 @@ function LogBookTitles(titles) {
         console.log(title);
     }
 }
-const poetryBooks = GetBookTitlesByCategory(Category.Poetry);
-LogBookTitles(poetryBooks);
+function GetBookById(id) {
+    const allBooks = GetAllBooks();
+    return allBooks.filter(book => book.id === id)[0];
+}
+//*********************************************************************************
+const fictionBooks = GetBookTitlesByCategory(Category.Fiction);
+fictionBooks.forEach((val, idx, arr) => console.log(++idx + ' - ' + val));
 //# sourceMappingURL=app.js.map
--- TSDemo-LibraryManagerApp/app.ts
+++ TSDemo-LibraryManagerApp/app.ts
@@ -1,9 +1,9 @@
 ﻿function GetAllBooks() {
     let books = [
-        { title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
-        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
-        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
-        { title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
+        { id: 1, title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+        { id: 2, title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { id: 3, title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+        { id: 4, title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
     ];
 
     return books;
@@ -48,5 +48,12 @@ function LogBookTitles(titles: Array<string>): void {
     }
 }
 
-const poetryBooks = GetBookTitlesByCategory(Category.Poetry);
-LogBookTitles(poetryBooks);
+function GetBookById(id: number) {
+    const allBooks = GetAllBooks();
+    return allBooks.filter(book => book.id === id)[0];
+}
+
+//*********************************************************************************
+
+const fictionBooks = GetBookTitlesByCategory(Category.Fiction);
+fictionBooks.forEach((val, idx, arr) => console.log(++idx + ' - ' + val));
